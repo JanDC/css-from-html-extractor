cf54b85ae812cc0d5a1c1851e8c12e3baf0d9c6b
Jan Decavele <jandecavele@gmail.com>
2016-10-22 09:47:59 +0200

    removed css-to-inline in favor of css-selector

---
+++ src/CssStore.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PageSpecificCss;
+
+class CssStore
+{
+    /** @var string[] */
+    private $styles = [];
+
+    public function addCssStyle($cssRules)
+    {
+        $this->styles[] = $cssRules;
+    }
+
+    public function getStyles()
+    {
+        return $this->styles;
+    }
+
+    public function compileStyles()
+    {
+        return join(';', $this->styles);
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return bool whether the dumping was successful
+     */
+    public function dumpStyles($path)
+    {
+        return file_put_contents($path, $this->compileStyles()) === false;
+    }
+
+
+}
--- src/PageSpecificCss.php
+++ src/PageSpecificCss.php
@@ -3,14 +3,44 @@
 namespace PageSpecificCss;
 
 use TijsVerkoyen\CssToInlineStyles\Css\Processor;
+use TijsVerkoyen\CssToInlineStyles\Css\Rule\Rule;
 use TijsVerkoyen\CssToInlineStyles\CssToInlineStyles;
 
 class PageSpecificCss extends CssToInlineStyles
 {
 
-    public function extractCss($page)
+    /**
+     * @var CssStore
+     */
+    private $cssStore;
+
+    /**
+     * PageSpecificCss constructor.
+     */
+    public function __construct()
     {
-        $processor = new Processor();
-        return $processor->getRules($processor->getCssFromStyleTags($page));
+        parent::__construct();
+
+        $this->cssStore = new CssStore();
+
+    }
+
+    /**
+     * @param string $html the raw html
+     */
+    public function processHtmlToStore($html)
+    {
+        $this->cssStore->addCssStyle($this->extractCss($html));
+    }
+
+    /**
+     * @param $html
+     *
+     * @return string
+     */
+    public function extractCss($html)
+    {
+        // Do something..
+        return '';
     }
 }
