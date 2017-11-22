f60523e4bd0f4f36a536a11c7fdfd4224046a581
Tom Van Looy <tom@ctors.net>
2017-11-22 15:46:33 +0100

    Restore interface

--- src/Twig/Extension.php
+++ src/Twig/Extension.php
@@ -5,8 +5,9 @@ namespace CSSFromHTMLExtractor\Twig;
 use CSSFromHTMLExtractor\CssFromHTMLExtractor;
 use CSSFromHTMLExtractor\Twig\TokenParsers\FoldTokenParser;
 use Twig_Extension;
+use Twig_ExtensionInterface;
 
-class Extension extends Twig_Extension
+class Extension extends Twig_Extension implements Twig_ExtensionInterface
 {
 
     /** @var CssFromHTMLExtractor */
